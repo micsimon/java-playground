8291a19651f7e3c3949346282484178f68789447
Michael Simon <5222293+micsimon@users.noreply.github.com>
2020-05-10 15:15:30 +0200

    test new intellij javadoc feature

--- src/main/java/common/AssertPlayground.java
+++ src/main/java/common/AssertPlayground.java
@@ -6,8 +6,17 @@ public class AssertPlayground {
         new AssertPlayground().doAssert(null);
     }
 
+
+    /**
+     * Hellps with some stuff
+     *
+     * @param value so foo
+     * @throws NullPointerException
+     */
     private void doAssert(String value) {
         assert value != null;
+
+        System.out.println("Hallo alls ok?");
 
         System.out.println("hey " + value);
     }
